5fe8201c603869c92600b47ab04c4b906ed48532
itsmeabhishekn <abhishekn4454@gmail.com>
2022-12-06 15:21:42 +0530

    added data to database

--- employee_backend/src/main/java/com/example/employee_backend/controller/employeeController.java
+++ employee_backend/src/main/java/com/example/employee_backend/controller/employeeController.java
@@ -1,46 +1,66 @@
 package com.example.employee_backend.controller;
 
+import com.example.employee_backend.dao.EmployeeDao;
 import com.example.employee_backend.model.Employees;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class employeeController {
 
+    @Autowired
+    private EmployeeDao dao;
+
+    @CrossOrigin(origins = "*")
     @GetMapping("/")
     public String homepage()
     {
         return "this is the homepage ";
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
     public String add(@RequestBody Employees e)
     {
 
         System.out.println(e.getName().toString());
+        System.out.println(e.getCompanyname().toString());
+        System.out.println(e.getDesignation());
+        System.out.println(e.getEmpcode());
+        System.out.println(e.getMobile());
+        System.out.println(e.getPassword());
+        System.out.println(e.getUsername());
+        System.out.println(e.getSalary());
+        dao.save(e);
         return "this is the add page ";
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/edit")
     public String edit()
     {
         return "this is the edit page ";
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/search")
     public String search()
     {
         return "this is the search page ";
     }
 
-    @GetMapping("viewall")
-    public String viewAll()
+    @CrossOrigin(origins = "*")
+    @GetMapping("/viewall")
+    public List<Employees> viewAll()
     {
-        return "this is the viewall page";
+
+
+        return (List<Employees>) dao.findAll();
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("delete")
     public String delete()
     {
